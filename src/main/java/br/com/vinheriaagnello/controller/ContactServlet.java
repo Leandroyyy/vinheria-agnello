393cb8a34789c8e062e89f8625d2da78ea5b2958
leandroyyy <leandro.cavallari123@gmail.com>
2025-04-27 17:52:00 -0300

    feat: servlet being used

--- src/main/java/br/com/vinheriaagnello/controller/ContactServlet.java
+++ src/main/java/br/com/vinheriaagnello/controller/ContactServlet.java
@@ -11,60 +11,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
-@WebServlet("/")
+@WebServlet("/sendContact")
 public class ContactServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private DynamoDbClient dynamoDb;
+  // private DynamoDbClient dynamoDb;
 
-    @Override
-    public void init() throws ServletException {
-    	System.out.println("Iniciado servlet");
-    	AwsBasicCredentials awsCreds = null;
+  @Override
+  public void init() throws ServletException {
+    System.out.println("Iniciado servlet");
 
-        dynamoDb = DynamoDbClient.builder()
-                .region(Region.SA_EAST_1) // ou outra região se sua tabela estiver em outro lugar
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
+    // dynamoDb = DynamoDbClient.builder()
+    // .region(Region.SA_EAST_1) // ou outra região se sua tabela estiver em outro
+    // lugar
+    // .build();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    System.out.println("Iniciado post");
+    String name = request.getParameter("name");
+    String email = request.getParameter("email");
+    String message = request.getParameter("message");
+
+    // Gera ID aleatório
+    String id = UUID.randomUUID().toString();
+
+    Map<String, AttributeValue> item = new HashMap<>();
+    item.put("id", AttributeValue.fromS(id));
+    item.put("name", AttributeValue.fromS(name));
+    item.put("email", AttributeValue.fromS(email));
+    item.put("message", AttributeValue.fromS(message));
+
+    PutItemRequest putReq = PutItemRequest.builder()
+        .tableName("ContactMessages")
+        .item(item)
+        .build();
+
+    try {
+      // dynamoDb.putItem(putReq);
+      response.setContentType("text/html");
+      response.getWriter().println("<h3>Mensagem enviada com sucesso!</h3>");
+    } catch (Exception e) {
+      response.setContentType("text/html");
+      response.getWriter().println("<h3>Erro ao enviar mensagem: " + e.getMessage() + "</h3>");
+      e.printStackTrace();
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    	System.out.println("Iniciado post");
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
-        String message = request.getParameter("message");
-
-        // Gera ID aleatório
-        String id = UUID.randomUUID().toString();
-
-        Map<String, AttributeValue> item = new HashMap<>();
-        item.put("id", AttributeValue.fromS(id));
-        item.put("name", AttributeValue.fromS(name));
-        item.put("email", AttributeValue.fromS(email));
-        item.put("message", AttributeValue.fromS(message));
-
-        PutItemRequest putReq = PutItemRequest.builder()
-                .tableName("ContactMessages")
-                .item(item)
-                .build();
-
-        try {
-            dynamoDb.putItem(putReq);
-            response.setContentType("text/html");
-            response.getWriter().println("<h3>Mensagem enviada com sucesso!</h3>");
-        } catch (Exception e) {
-            response.setContentType("text/html");
-            response.getWriter().println("<h3>Erro ao enviar mensagem: " + e.getMessage() + "</h3>");
-            e.printStackTrace();
-        }
-    }
+  }
 }
